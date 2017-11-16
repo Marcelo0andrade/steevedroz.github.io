a8868b858ea1ae93b822743964b40b9342641061
da Silva Andrade Marcelo <daSilvaM28@s2.rpn.ch>
2017-11-16 13:40:59 +0100

    My Button is mybe nice

--- color.js
+++ color.js
@@ -1,22 +1,25 @@
-/*$(function() {
-  $('#day').click(function() {
-    const day = $(this).attr('id')
-  })
-})
-$("#day").focus(function(){
-    $(this).parent().css("background", "blue"*/
-//test
-function check() {
-    document.getElementById("day").checked = true;
+/*function check() {
+  document.getElementById("#day").checked = true;
 }
 
 function uncheck() {
-    document.getElementById("day").checked = false;
+  document.getElementById("#day").checked = false;
 }
-
-if (day=true) {
-  function changeBackground(blue) {
-     document.body.style.background = grey;
-  }
-
+console.log('MAINTENANT');
+if (day) {
+    console.log('APRES');
+    function changeBackground(color) {
+    document.body.style.background = grey
 }
+*/
+function DayOrNot()
+{
+   var night = document.getElementById('day')
+
+   night.onclick = function()
+   {
+     alert("HI 2+2?")
+    return false
+   };
+}
+window.onload = DayOrNot
