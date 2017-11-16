eb1f2ca3a101291369a4be23e04e11785e603e4b
da Silva Andrade Marcelo <daSilvaM28@s2.rpn.ch>
2017-11-16 10:45:58 +0100

    try to do a button

---
+++ color.js
@@ -0,0 +1,22 @@
+/*$(function() {
+  $('#day').click(function() {
+    const day = $(this).attr('id')
+  })
+})
+$("#day").focus(function(){
+    $(this).parent().css("background", "blue"*/
+//test
+function check() {
+    document.getElementById("day").checked = true;
+}
+
+function uncheck() {
+    document.getElementById("day").checked = false;
+}
+
+if (day=true) {
+  function changeBackground(blue) {
+     document.body.style.background = grey;
+  }
+
+}
